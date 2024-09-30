d0edb6c7acafcfee61f9445afd3941e0fcbdb915
Pen9uin <sirpen9uin@gmail.com>
2024-09-30 22:00:29 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# HelpDesk: Frontend
+
+[![Build status](https://ci.appveyor.com/api/projects/status/d29ftsf2u3q19ywr?svg=true)](https://ci.appveyor.com/project/sirpen9uin/ahj_frontend)
+
+## Описание
+
+[Домашнее задание к занятию "7. Работа с HTTP"](https://github.com/netology-code/ahj-homeworks/tree/AHJ-50/http#helpdesk-frontend)
+
+## Развертывание
+
+```npm install```
+
+```npm run dev```
+
+## Сборка
+
+```npm run build```
---
+++ src/js/base-service.js
@@ -0,0 +1,28 @@
+// this class must be abstract
+export default class BaseService {
+  constructor(baseURL) { this._baseURL = baseURL; }
+
+  get(url) { return BaseService.fetch(this._baseURL + url, { method: 'GET' }); }
+
+  post(url, data = new FormData()) { return BaseService.fetch(this._baseURL + url, { method: 'POST', body: data }); }
+
+  static async fetch(input, init) {
+    try {
+      const response = await fetch(input, init);
+
+      if (response.ok) {
+        if (response.status === 204) {
+          return { status: response.status };
+        }
+
+        return await response.json();
+      }
+      else {
+        console.error('Ошибка:', await response.json());
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+}
